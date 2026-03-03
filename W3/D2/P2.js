c13d3d66683218ee8c7a3db83ca1ff9e328d6f9d
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-03 15:45:48 +0530

    Comparison operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+// Comparison operators
+
+console.log("Comparison operators");
+let x=3,y="3";
+console.log("x==y:",x==y); //loose equality
+console.log("x===y:",x===y); //strict equality
+console.log("x!=y:",x!=y); //loose inequality
+console.log("x!==y:",x!==y); //strict inequality
+console.log("x>3:",x>3);
+console.log("x<3:",x<3);
+console.log("x>=3:",x>=3);
+console.log("x<=3:",x<=3);
