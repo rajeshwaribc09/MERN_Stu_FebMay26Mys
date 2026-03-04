87eeb56a47e8cf3a13dcc00341c032280f2ae8da
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-04 11:31:23 +0530

    arrow functions

---
+++ W3/D2/P7.js
@@ -0,0 +1,17 @@
+// arrow functions
+(args) => {
+    // body of the function
+}
+
+// Add two numbers
+const Sum=(a,b) => {
+    return a+b;
+}
+console.log("3+5=",Sum(3,5));
+
+// Single line return/implicit return
+const square = x=>x*x;
+console.log("square of 44:",square(44));
+
+const sayHello=()=>console.log("Hello from arrow function.");
+sayHello();
