88ba67ffd6247a403e44d35dd95bf02e7bb63891
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-04 14:36:49 +0530

    Object iteration

---
+++ W3/D3/P7.js
@@ -0,0 +1,12 @@
+// Object iteration
+const book={
+    title:"JS for newbies",
+    author:"Rajeshwari",
+    year:2026
+};
+for(let key in book){
+    console.log(key,":",book[key]);
+}
+console.log("keys",Object.keys(book));
+console.log("values",Object.values(book));
+console.log("values",Object.entries(book));
