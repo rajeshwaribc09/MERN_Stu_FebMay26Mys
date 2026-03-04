4f2eac3c4e3f8e0caa5c37b4e314d03e2e763413
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-04 14:35:54 +0530

    basics of obects,modify,delete,object constructor

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -4,6 +4,23 @@ const person={
     age:30,
     isStudent:false
 };
+// console.log("Person:",person);
+// console.log("Name:",person.name);
+// console.log("Age:",person["age"]);
+
+// add a new property
+person.city="mysore";
 console.log("Person:",person);
-console.log("Name:",person.name);
-console.log("Age:",person["age"]);
+// modify
+person.age=21;
+console.log("Person:",person);
+// delete
+delete person.isStudent;
+console.log("Person:",person);
+
+// object constructor
+const car=new Object();
+car.make="Audi";
+car.model="A4";
+car.year=2026;
+console.log(car);
