0f827938965ff5e5a458fb7f760429183220be11
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-04 11:23:01 +0530

    Function

---
+++ W3/D2/P5.js
@@ -0,0 +1,9 @@
+// Function Declaration
+// function greet(name);  "this is only in the case of c pgming"
+greet("Rajeshwari B C");  //hoisting
+// Function Declaration/Definition
+function greet(name){
+    console.log("Hello,"+name+" !");
+}
+// function call
+// greet("Rajeshwari B C");
