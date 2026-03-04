b6dd006a21654e6fa9a281eda1f838060ddcab0f
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-04 12:37:41 +0530

    while,do while

---
+++ W3/D3/P2.js
@@ -0,0 +1,15 @@
+// while loop
+console.log("while loop");
+let count=0;
+while(count<5){
+    console.log("count:",count);
+    count++;
+}
+
+// do while loop
+console.log("do while loop");
+let value=0;
+do{
+    console.log("value:",value);
+    value++;
+}while(value<3)
