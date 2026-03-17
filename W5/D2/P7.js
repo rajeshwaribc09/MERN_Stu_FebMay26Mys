b270425ec7994e0c7f290a9fa659030622df8fdd
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-17 14:36:49 +0530

    callback handling with named functions

---
+++ W5/D2/P7.js
@@ -0,0 +1,31 @@
+function loadUser(next){
+    setTimeout(function(){
+        console.log("step 1:User loaded.");
+        next();
+    },400);
+}
+function loadOrders(next){
+    setTimeout(function(){
+        console.log("step 2:Orders loaded.");
+        next();
+    },400);
+}
+function loadPayments(next){
+    setTimeout(function(){
+        console.log("Step 3:payments loaded.");
+        next();
+    },400);
+}
+function loadShipment(){
+    setTimeout(function(){
+        console.log("Step 4:shipment loaded.");
+        console.log("Same flow but easier to read");
+    },400);
+}
+loadUser(function(){
+    loadOrders(function(){
+        loadPayments(function(){
+            loadShipment();
+        });
+    });
+});
