fee2effcd45159aafe466793777450039a4fbf38
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-23 12:34:18 +0530

    Using custom commonJS module

---
+++ W6/D1/P3.js
@@ -0,0 +1,13 @@
+// Using custom commonJS module [custom module-means it is created by us]
+const {calculateTax,applyDiscount,formatCurrency}=require("./P2");
+const itemName="Laptop";
+const basePrice=60000;
+const discountedPrice= applyDiscount(basePrice,10);
+const taxAmount=calculateTax(discountedPrice);
+const finalPrice=discountedPrice+taxAmount;
+
+console.log("Item: ",itemName);
+console.log("Base price: ",formatCurrency(basePrice));
+console.log("Discounted Price: ",formatCurrency(discountedPrice));
+console.log("GST Tax @18%: ",formatCurrency(taxAmount));
+console.log("Final Price: ",formatCurrency(finalPrice));
