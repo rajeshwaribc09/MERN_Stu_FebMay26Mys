ff653d4783852714737645dfbc71d6d5266c9453
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-30 11:35:54 +0530

    Handling different GET routes

---
+++ W6/D4/P4.js
@@ -0,0 +1,38 @@
+// Handling different GET routes
+const http=require("http");
+const server=http.createServer(function(req,res){
+    if(req.method==="GET" && req.url==="/"){
+        res.writeHead(200,{"content-type":"text/plain"});
+        res.end("Home page/Dashboard");
+        return;
+    }
+    if(req.method==="GET" && req.url==="/about"){
+        res.writeHead(200,{"content-type":"text/plain"});
+        res.end("About Route.Welcome to About us page");
+        return;
+    }
+    if(req.method==="GET" && req.url==="/products"){
+        res.writeHead(200,{"content-type":"text/plain"});
+        res.end("About Route.Welcome to products page");
+        return;
+    }
+    if(req.method==="GET" && req.url==="/users"){
+        res.writeHead(200,{"content-type":"text/plain"});
+        res.end("Returning all users.");
+        return;
+    }
+    // POST=create
+    // curl -X POST http://localhost:3001/users
+    // curl:Client URL:free,open src cli tool used to transfer data to or from server using various network protocol. 
+    if(req.method==="POST" && req.url==="/users"){
+        res.writeHead(201,{"content-type":"text/plain"});
+        res.end("New users created");
+        return;
+    }
+    // unknown route fallback
+    res.writeHead(404,{"content-type":"text/plain"});
+    res.end("Route not found.");
+});
+server.listen(3001,function(){
+    console.log("Server is running at http://localhost:3001");
+});
