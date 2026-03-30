39e3ca1251360cd7e8fd1cb6ffe05f3f7156a933
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-03-30 11:13:39 +0530

    Basics of Express-setup

---
+++ W7/D1/P1.js
@@ -0,0 +1,19 @@
+// Basics of Express-setup
+// npm init -y
+// npm install express
+
+// import module of express
+const express=require("express");
+// calling express() creates the main application object
+// This object is used to register routes and middleware
+const app=express();
+// app.get() handles GET requests for a specific path
+app.get("/",function(req,res){
+    // res.send() sends a response body and ends the request automatically
+    res.send("Hello from express server");
+});
+
+// listen() starts the server on a chosen port number
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
