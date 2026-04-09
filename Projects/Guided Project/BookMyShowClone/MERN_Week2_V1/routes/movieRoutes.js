d06398acb2569ce98bf02ff853257fd4313d1a31
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-04-09 10:12:57 +0530

    Handles requests related to movie

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/movieRoutes.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/movieRoutes.js
@@ -18,10 +18,10 @@ router.get("/movies",getAllMovies);
 // gets the movie by its Id
 router.get("/movies/:id",getMovieById);
 // sends request to create movie
-router.post("/movies",authMiddleware,roleMiddleware("admin").addMovie);
+router.post("/movies",authMiddleware,roleMiddleware("admin"),addMovie);
 // sends request to update movie detail's
-router.put("/movies/:id",authMiddleware,roleMiddleware("admin").updateMovie);
+router.put("/movies/:id",authMiddleware,roleMiddleware("admin"),updateMovie);
 // sends request to delete a movie
-router.delete("/movies/:id",authMiddleware,roleMiddleware("admin").deleteMovie);
+router.delete("/movies/:id",authMiddleware,roleMiddleware("admin"),deleteMovie);
 
 module.exports=router;
