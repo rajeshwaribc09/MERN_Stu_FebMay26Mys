60a903161f24b502df8544bbeaa891416a1e96eb
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-04-09 11:23:48 +0530

    Express app configuration file

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/app.js
@@ -1,25 +1,31 @@
 // Express app configuration file
-const express=require("express");
-const cookieParser=require("cookie-parser");
-const session=require("express-session");
-const loggerMiddleware=require("./middleware/loggerMiddleware");
-const errorMiddleware=require("./middleware/errorMiddleware");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const loggerMiddleware = require("./middleware/loggerMiddleware");
+const errorMiddleware = require("./middleware/errorMiddleware");
 
-const movieRoutes=require("./routes/movieRoutes");
-const authRoutes=require("./routes/authRoutes");
-const bookingRoutes=require("./routes/bookingRoutes");
-const userRoutes=require("./routes/userRoutes");
+const movieRoutes = require("./routes/movieRoutes");
+const authRoutes = require("./routes/authRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(session({
-    secret:"secret"
+    secret: "secretKeyshhhdonttellanyone",
+    resave: false,
+    saveUnitialized: false,
+    cookie: {
+        secure: false,
+        httpOnly: true
+    }
 }));
 app.use(loggerMiddleware);
-app.use("/",movieRoutes);
-app.use("/auth",authRoutes);
-app.use("/users",userRoutes);
+app.use("/", movieRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 app.use(errorMiddleware);
-module.exports=app;
+module.exports = app;
