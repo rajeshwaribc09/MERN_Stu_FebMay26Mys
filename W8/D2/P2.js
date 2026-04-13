a471dc5acf34245d634b52c03d9bf8dc04aaa6b9
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-04-13 10:25:39 +0530

    Indexing for performance improvement

---
+++ W8/D2/P2.js
@@ -0,0 +1,17 @@
+// Indexing for performance improvement
+const mongoose = require("mongoose");
+const { type } = require("node:os");
+const employeeSchema=new mongoose.Schema({
+    email:{type:String,index:true},
+    username:{type:String,unique:true},
+    role:String,
+    createdAt:Date
+});
+
+// Compound index
+// Query filter by role and sort by createdAt
+employeeSchema.index({role:1,createdAt:-1});
+
+const Employee=mongoose.model("IndexedEmployee",employeeSchema);
+console.log("Compound index:");
+console.log("{role:1,createdAt:-1}");
