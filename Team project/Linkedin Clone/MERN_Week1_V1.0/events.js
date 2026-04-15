a9e130df49982695bdbabeaf4db387c5e47b8832
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-04-15 14:33:41 +0530

    Custom EventEmitter instance for system events

--- Team project/Linkedin Clone/MERN_Week1_V1.0/events.js
+++ Team project/Linkedin Clone/MERN_Week1_V1.0/events.js
@@ -3,9 +3,12 @@ const chalk = require("chalk");
 
 const emitter = new EventEmitter();
 
+// SESSION
 emitter.on("sessionStarted", (user) => {
   console.log(chalk.blue(`Logged in as ${user.name}`));
 });
+
+// PROFILE
 emitter.on("profileCreated", (user) => {
   console.log(chalk.green(`Profile created successfully: ${user.name}`));
 });
@@ -13,8 +16,36 @@ emitter.on("profileCreated", (user) => {
 emitter.on("profileUpdated", () => {
   console.log(chalk.yellow("Profile updated successfully"));
 });
+
+// CONNECTIONS
+emitter.on("connectionRequestSent", () => {
+  console.log(chalk.green("Connection request sent"));
+});
+
+emitter.on("connectionAccepted", () => {
+  console.log(chalk.green("Connection accepted"));
+});
+
+emitter.on("connectionRejected", () => {
+  console.log(chalk.yellow("Connection rejected"));
+});
+
+// ERROR
 emitter.on("operationFailed", (msg) => {
   console.log(chalk.red(`Error: ${msg}`));
+});
+
+// Posts
+emitter.on("postCreated", () => {
+  console.log(chalk.green("Post created successfully"));
+});
+
+emitter.on("postLiked", () => {
+  console.log(chalk.blue("Post liked"));
+});
+
+emitter.on("commentAdded", () => {
+  console.log(chalk.yellow("Comment added"));
 });
 
 module.exports = emitter;
