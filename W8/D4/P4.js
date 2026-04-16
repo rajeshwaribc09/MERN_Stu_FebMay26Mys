4f9f34e754ea4f7e451ba54517b0977426bf7b1f
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-04-16 11:50:15 +0530

    OTP generation using Crypto built in module of Node JS

---
+++ W8/D4/P4.js
@@ -0,0 +1,8 @@
+// OTP generation using Crypto built in module of Node JS
+const crypto=require('crypto');
+function generateOTP(length=6){
+    return crypto.randomInt(0,10**length)
+    .toString()
+    .padStart(length,'0');
+}
+console.log("OTP:",generateOTP());
