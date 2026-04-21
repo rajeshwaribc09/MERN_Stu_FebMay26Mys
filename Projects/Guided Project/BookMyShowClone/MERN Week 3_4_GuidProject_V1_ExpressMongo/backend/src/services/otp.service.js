c7860de0aea37fdb15ae4477d74469ffbcbeba15
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-04-21 14:59:43 +0530

    service function related to OTP generation created

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/otp.service.js
@@ -0,0 +1,18 @@
+const OTP=require("../models/OTP");
+const bcrypt=require("bcrypt");
+
+// Generate OTP
+exports.generateOTP=async(email)=>{ 
+    const otp=Math.floor(100000+Math.random()*900000).toString();    //task is convert this line using crypto to generate the otp
+    const hashedOtp=await bcrypt.hash(otp,10);
+    const expiresAt=new Date(Date.now() + 5*60*1000);
+
+    await OTP.create({
+        email,
+        otp:hashedOtp,
+        expiresAt,
+    });
+
+    console.log("Generated OTP: ",otp); //testing only
+    return otp;
+};
