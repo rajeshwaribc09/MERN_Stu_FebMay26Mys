59027dc29757793af78efb6898c40532024e3e3e
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-04-28 15:31:39 +0530

    Props basics react app created

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { PropBasics } from './components/P1'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <PropBasics/>
+    </>
+  )
+}
+
+export default App
