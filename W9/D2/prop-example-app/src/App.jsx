c9c9abb72f16f6574630303ed5a47d6d93de4e14
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-04-29 11:44:01 +0530

    State Lifting

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import { PropBasics } from './components/P1'
+import { PropDestructuring } from './components/P2'
+import { SharedStateParent } from './components/P5'
+import { PropsChildren } from './components/P3'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <PropBasics/>
+      {/* <PropBasics/> */}
+      {/* <PropDestructuring/> */}
+      {/* <PropsChildren/> */}
+      <SharedStateParent/>
     </>
   )
 }
