75b8475c063bd44d8ebe8316b1f596b60e857fff
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-05-04 10:46:50 +0530

    useState basics

---
+++ W10/D1/state-hooks-app/src/components/P1.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+// useState basics
+// Its a react hook that adds state to functional component
+// Returns an array with 2 values
+// 1.Current state value
+// 2.Function to update state
+// Syntax:
+// const [stateValue,setStateValue]=useState(initialValue);
+export function UseStateBasics(){
+    // counter
+    const [count,setCount]=useState(0);
+    // cart
+    // const [cart,setCart]=useState(0);
+    return(
+        <>
+        <h2>useState Basics</h2>
+        <p>Count:{count}</p>
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <button onClick={()=>setCount(count-1)}>Decrement</button>
+        </>
+    )
+}
