81a49653d7fc5fbccead9fcb10e93804a377e89d
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-05-07 14:49:30 +0530

    Common from patterns

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -2,13 +2,19 @@ import './App.css'
 import { EventBasics } from "./components/P1";
 import { EventObject } from "./components/P2";
 import { PassingArguments } from './components/P3';
+import { UseCallbackEvents } from './components/P5';
+import { EventDelegation } from './components/P6';
+import { ControlledComponent } from './components/P7';
 
 function App() {
    return (
     <>
       {/* <EventBasics /> */}
       {/* <EventObject /> */}
-      <PassingArguments/>
+      {/* <PassingArguments/> */}
+      {/* <UseCallbackEvents/> */}
+      {/* <EventDelegation/> */}
+      <ControlledComponent/>
     </>
   )
 }
