b140c326d8effb9bc8bb11397ef1df817fa44fa0
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-05-14 12:39:08 +0530

    Form handling

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,22 +1,30 @@
-const express=require("express");
-const authRoutes=require("./src/routes/auth.routes");
-const movieRoutes=require("./src/routes/movie.routes");
-const bookingRoutes=require("./src/routes/booking.routes");
-const showRoutes=require("./src/routes/show.routes");
+const express = require("express");
+const authRoutes = require("./src/routes/auth.routes");
+const movieRoutes = require("./src/routes/movie.routes");
+const bookingRoutes = require("./src/routes/booking.routes");
+const showRoutes = require("./src/routes/show.routes");
+const errorMiddleware = require("./src/middleware/error.middleware"); 
 
-const app=express();
+//REQUEST LOGGING 
+//app.js 
+const morgan = require("morgan"); 
+
+
+const app = express();
 
 app.use(express.json());
-app.use("/api/auth",authRoutes);
-app.use("/api/movies",movieRoutes);
-app.use("/api/bookings",bookingRoutes);
-app.use("/api/shows",showRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/movies", movieRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/shows", showRoutes);
+app.use(errorMiddleware);
+app.use(morgan("dev"));
 
 // Base URL
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.status(200).json({
-    success:true,
-    message:"Movie booking API is running.."
+        success: true,
+        message: "Movie booking API is running.."
+    });
 });
-});
-module.exports=app;
+module.exports = app;
