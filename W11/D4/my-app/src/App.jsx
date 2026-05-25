31b64de53c9e4cf9b640eb2f06c9c94e3a257138
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-05-25 13:13:22 +0530

    Async form submission

--- W11/D4/my-app/src/App.jsx
+++ W11/D4/my-app/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import { ControlledForm } from './components/P1'
 import { ValidationForm } from './components/P2'
+import { AsyncForm } from './components/P3'
 function App() {
 
   return (
     <>
       {/* <ControlledForm/> */}
-      <ValidationForm/>
+      {/* <ValidationForm/> */}
+      <AsyncForm/>
     </>
   )
 }
