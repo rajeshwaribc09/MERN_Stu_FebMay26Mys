ff3d5be03f39cdc02315a34a6cccf41a18b81055
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-06-12 15:15:47 +0530

    Conditional rendering

---
+++ W12/Final Frontend Project/src/components/MovieResults.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+export default function MovieResults() {
+    const [movies, setMovies] = useState([]);
+    return (
+        <section>
+            <h2>Search results</h2>
+            {movies.length === 0 ? <p>No movies found</p> :
+                <ul>
+                    {movies.map((movie)=>(<li key={movie}>{movie}</li>))}
+                </ul>}
+                <button onClick={()=>{
+                    setMovies(["Inception","Dhurandar","XYZ"]);
+                }}>Load Movies</button>
+        </section>
+    )
+}
