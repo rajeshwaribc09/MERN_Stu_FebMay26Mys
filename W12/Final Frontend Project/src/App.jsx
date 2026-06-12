7a3e08670e98cb44ad64dc5ac34c6e99c6424b41
rajeshwaribc09 <rajeshwaribc09@gmail.com>
2026-06-12 12:04:50 +0530

    Usage of props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,9 +1,31 @@
 import './App.css'
-import HomePage from './pages/HomePage'
+import MovieCard from './components/MovieCard';
 function App() {
   return (
     // Component composition
-    <HomePage/>
+    <>
+    <MovieCard
+       title="Inception"
+       genre="Sci-Fi"
+       rating={4.8}
+       duration="2h 28m"
+       poster="https://picsum.photos/200/300?1"
+    />
+    <MovieCard
+       title="Avatar"
+       genre="Sci-Fi"
+       rating={4.1}
+       duration="3h 28m"
+       poster="https://picsum.photos/200/300?2"
+    />
+    <MovieCard
+       title="FightClub"
+       genre="Sci-Fi"
+       rating={4.9}
+       duration="2h 28m"
+       poster="https://picsum.photos/200/300?3"
+    />
+    </>
   );
 }
 
